5473d8d5df9c96d079b95ea4ddea1fe691366a1a
michaelxuw <michaelxuwrelated@gmail.com>
2022-04-26 11:43:29 +0200

    command feature to be pushed

---
+++ src/main/java/dat/eksamensProject/control/CommandByMichael.java
@@ -0,0 +1,13 @@
+package dat.eksamensProject.control;
+
+import dat.eksamensProject.model.exceptions.DatabaseException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class CommandByMichael extends Command{
+    @Override
+    String execute(HttpServletRequest request, HttpServletResponse response) throws DatabaseException {
+        return null;
+    }
+}
